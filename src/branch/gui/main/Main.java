662cad822231717923caffae00889705e6f6a3d8
haney <hansduran25@gmail.com>
2023-09-10 16:48:56 +0800

    Exchange lines

--- src/branch/gui/main/Main.java
+++ src/branch/gui/main/Main.java
@@ -2,7 +2,7 @@ package branch.gui.main;
 
 public class Main {
 	public static void main(String[] args) {
-		var mainWin = new MainWindow();
 		System.out.println("GUI Test");
+		var mainWin = new MainWindow();
 	}
 }
